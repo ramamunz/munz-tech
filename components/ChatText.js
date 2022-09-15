1ef2a2e54d63101daa2b2b4ebeb97eb67c53c81f
Muhammad Ramadhan Muna <ramamuna08@gmail.com>
2022-09-16 00:32:22 +0700

    your first commit

---
+++ components/ChatText.js
@@ -0,0 +1,7 @@
+export default function ChatText({ children }) {
+  return (
+    <div className="mt-4 px-6 py-3 text-left border rounded-xl shadow-sm bg-white">
+      {children}
+    </div>
+  );
+}
